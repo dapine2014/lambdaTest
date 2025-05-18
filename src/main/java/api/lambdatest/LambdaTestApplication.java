6e8d96f0f5a73437031340b6c0d84749cc6ac7ac
alex <dapine@gmail.com>
2025-05-18 12:30:11 -0500

    primer commit

--- src/main/java/api/lambdatest/LambdaTestApplication.java
+++ src/main/java/api/lambdatest/LambdaTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LambdaTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LambdaTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LambdaTestApplication.class, args);
+    }
 
 }
--- src/test/java/api/lambdatest/LambdaTestApplicationTests.java
+++ src/test/java/api/lambdatest/LambdaTestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LambdaTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
