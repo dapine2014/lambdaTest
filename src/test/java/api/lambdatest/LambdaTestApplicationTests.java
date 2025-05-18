7f67e43a36e377f39cf24817f74401a4309b4de0
alex <dapine@gmail.com>
2025-05-18 12:28:59 -0500

    Initial commit

---
+++ src/test/java/api/lambdatest/LambdaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package api.lambdatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
